116d21ef195e8d207749f3c3b1add2503bd580f4
paolopci <paolopci@yahoo.it>
2020-02-25 16:23:45 +0100

    Chapter 5 initial Web service

--- src/app/models/repository.ts
+++ src/app/models/repository.ts
@@ -1,9 +1,25 @@
 import { Product } from "./product.model";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
+
+@Injectable()
 export class Repository {
-  product: Product;
+  productData: Product;
 
-  constructor() {
-    this.product = JSON.parse(document.getElementById("data").textContent);
+  constructor(private http: HttpClient) {
+    this.getProduct(1);
+  }
+
+  getProduct(id: number) {
+    this.http.get<Product>("/api/products/" + id).subscribe(p => {
+      this.productData = p;
+      console.log("Product Data Received");
+    });
+  }
+
+  get product(): Product {
+    console.log("Product Data Requested");
+    return this.productData;
   }
 }
