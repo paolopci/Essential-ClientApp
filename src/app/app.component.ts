4061f375be0ecc11fd7eab1b574b77d43d9afd50
paolopci <paolopci@yahoo.it>
2020-02-27 16:21:39 +0100

    Chapter 6 Implementing Create , Update , Replace Products and Supplier ClientApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Repository } from "./models/repository";
 import { Product } from "./models/product.model";
+import { Supplier } from "./models/supplier.model";
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import { Product } from "./models/product.model";
 })
 export class AppComponent {
 
-  constructor(private repo: Repository) { }
+  constructor(private repo: Repository) {}
 
   get product(): Product {
     return this.repo.product;
@@ -17,5 +18,32 @@ export class AppComponent {
 
   get products(): Product[] {
     return this.repo.products;
+  }
+
+  createProduct() {
+    this.repo.createProduct(new Product(0,
+      "X-Ray Scuba Mask",
+      "Watersports",
+      "See what the fish are hiding",
+      49.99,
+      this.repo.products[0].supplier));
+  }
+
+  createProductAndSupplier() {
+    let s = new Supplier(0, "Rocket Shoe Corp", "Pesaro", "IT");
+    let p = new Product(0, "Rocket-Powered Shoes", "Running", "Set a new record", 100, s);
+    this.repo.createProductAndSupplier(p, s);
+  }
+
+  replaceProduct() {
+    let prod = this.repo.products[0];
+    prod.name = "Modified Product";
+    prod.category = "Modified Category";
+    this.repo.replaceProduct(prod);
+  }
+
+  replaceSupplier() {
+    let supp = new Supplier(3, "Modified Supplier", "Pesaro ...", "IT");
+    this.repo.replaceSupplier(supp);
   }
 }
--- src/app/models/repository.ts
+++ src/app/models/repository.ts
@@ -1,30 +1,33 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Product } from "./product.model";
+import { Supplier } from "./supplier.model"
 import { Filter } from "./configClasses.repository";
 
 
-const productUrl = "/api/products";
+const productsUrl = "/api/products";
+const suppliersUrl = "/api/suppliers";
 
 
 @Injectable()
 export class Repository {
   product: Product;
   products: Product[];
+  suppliers: Supplier[]=[];
   filter: Filter=new Filter();
 
   constructor(private http: HttpClient) {
-    this.filter.category = "Garden";
+    // this.filter.category = "Garden";
     this.filter.related = true;
     this.getProducts();
   }
 
   getProduct(id: number) {
-    this.http.get<Product>(`${productUrl}/${id}`).subscribe(p => { this.product = p; });
+    this.http.get<Product>(`${productsUrl}/${id}`).subscribe(p => { this.product = p; });
   }
 
   getProducts() {
-    let url = `${productUrl}?related=${this.filter.related}`;
+    let url = `${productsUrl}?related=${this.filter.related}`;
     if (this.filter.category) {
       url += `&category=${this.filter.category}`;
     }
@@ -33,5 +36,66 @@ export class Repository {
     }
 
     this.http.get<Product[]>(url).subscribe(prods => this.products = prods);
+  }
+
+  getSuppliers() {
+    this.http.get<Supplier[]>(suppliersUrl).subscribe(supp => this.suppliers = supp);
+  }
+
+
+  createProduct(prod: Product) {
+    let data = {
+      name: prod.name,
+      category: prod.category,
+      description: prod.description,
+      price: prod.price,
+      supplier: prod.supplier ? prod.supplier.supplierId : 0
+    };
+
+    this.http.post<number>(productsUrl, data).subscribe(id => {
+      prod.productId = id;
+      this.products.push(prod);
+    });
+  }
+
+  createProductAndSupplier(prod: Product, supp: Supplier) {
+    let data = {
+      name: supp.name,
+      city: supp.city,
+      state: supp.state
+    };
+
+    this.http.post<number>(suppliersUrl, data).subscribe(id => {
+      supp.supplierId = id;
+      prod.supplier = supp;
+      this.suppliers.push(supp);
+      if (prod != null) {
+        this.createProduct(prod);
+      }
+    });
+  }
+
+
+  //  Replace Product
+  replaceProduct(prod: Product) {
+    let data = {
+      name: prod.name,
+      category: prod.category,
+      description: prod.description,
+      price: prod.price,
+      supplier: prod.supplier ? prod.supplier.supplierId : 0
+    };
+
+    this.http.put(`${productsUrl}/${prod.productId}`, data).subscribe(() => this.getProducts());
+  }
+
+  // Replace Supplier
+  replaceSupplier(supp: Supplier) {
+    let data = {
+      name: supp.name,
+      city: supp.city,
+      state: supp.state
+    };
+    this.http.put(`${suppliersUrl}/${supp.supplierId}`, data).subscribe(() => this.getProducts());
   }
 }
